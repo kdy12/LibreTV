3a620431c0acddb8e58188cc220013cba98fa42b
kdy12 <40226889+kdy12@users.noreply.github.com>
2026-01-11 15:21:20 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -159,11 +159,11 @@ const API_SITES = {
      },
      huangcang: {
     //     api: 'https://hsckzy.vip/api.php/provide/vod',
-    //     api: 'https://hsckzy888.com/api.php/provide/vod',
-         api: 'http://dadizy2.com/api.php/provide/vod',
+         api: 'https://hsckzy888.com/api.php/provide/vod',
+    //     api: 'http://dadizy2.com/api.php/provide/vod',
          name: '黄色仓库',
          adult: true,
-   //      detail: 'https://hsckzy.vip/api.php/provide/vod'
+         detail: 'https://hsckzy888.com/api.php/provide/vod'
      },
      yutu: {
          api: 'https://yutuzy10.com/api.php/provide/vod',
