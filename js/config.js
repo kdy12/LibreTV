a1916610dc961cfbea8fbc4ba799e6956fa61a9c
kdy12 <40226889+kdy12@users.noreply.github.com>
2026-01-11 15:31:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,6 +114,11 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
+    mtzy: {
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod/at/josn/',
+        name: '茅台资源'
+    },
+    
 //    testSource: {
 //        api: 'https://www.example.com/api.php/provide/vod',
 //        name: '空内容测试源',
